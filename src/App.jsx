e0c818e4f117169efbe43df65b16c0e7db743d5b
elisa <bascongarcia@gmail.com>
2022-08-08 17:34:57 +0200

    Iteration 1 y 2 Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 import tasks from './list.json';
+import TaskCard from './components/TaskCard';
 
 function App() {
+  const [task, setTask] = useState(tasks);
+
+  const handleDelete = (name) => {
+    const filtered = task.filter(elem => elem.name !==name);
+    setTask(filtered);
+  }
+
   return (
     <div className="App">
-
+    <h1>To do list</h1>
+    {task.map((elem) => {
+      return (
+        <TaskCard key={elem.name} task={elem} onDelete={handleDelete}/>
+      )
+    })}
     </div>
   );
 }
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TaskCard(props) {
+    const {task: {name, image, useful_link, urgency, description, done, }, onDelete} = props;
+    return (
+    <div>
+        <image src={image}></image>
+        <p>{urgency}</p>
+        <p>{name}</p>
+        <p>{description}</p>
+        <p>resources: {useful_link}</p>
+        <button onClick={() => onDelete(name)}>🗑️</button>
+    </div>
+    )
+}
