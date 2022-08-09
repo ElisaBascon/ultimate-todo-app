b37cbb57038d7fe88121d55bbf5a2308d6bf0c66
elisa <bascongarcia@gmail.com>
2022-08-09 12:33:15 +0200

    refactor links

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,11 +5,11 @@ export default function TaskCard(props) {
     return (
     <div className="cardTask">
     <button className="deleteButton" onClick={() => onDelete(name)}>✖</button>
-        <img src={image} alt="name" width="100%"/>
-        <h3> {urgency} -</h3>
-        <h3>{name}</h3>
+        <img src={image} alt="name"/>
+        <h2> {urgency} -</h2>
+        <h2>{name}</h2>
         <p>{description}</p>
-        <h6>⚡ {useful_link} ⚡</h6>
+        <a href={useful_link}>⚡ Resources ⚡</a>
         
     </div>
     )
